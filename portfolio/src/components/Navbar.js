109dc66eae1dddeda0c14f98e1b27c47fee6d992
Rene Ortega <reneortega2468@gmail.com>
2022-06-01 22:15:59 -0400

    test

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import resume from "../WebDev.pdf";
-import second from '../imgs/shutterstock_1387394786.png'
+import roLogo from '../imgs/logo.png'
 
 const Index = () => {
    return(
     <nav class=" border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
     <a href="https://flowbite.com" class="flex items-center">
-        <img src={second} class="mr-3 h-6 sm:h-9 rounded-lg" alt="Flowbite Logo" />
+        <img src={roLogo} class="mr-3 h-6 sm:h-9 rounded-lg" alt="Flowbite Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap text-white">Rene Ortega</span>
     </a>
     <div class="flex md:order-2">
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hidden">Get started</button>
+        {/* <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 hidden">Get started</button> */}
         <button data-collapse-toggle="mobile-menu-4" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -27,10 +27,10 @@ const Index = () => {
           <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+          <a target="_blank" href={resume} class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Resume</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+          <a target="_blank" href={resume} class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
         </li>
       </ul>
     </div>
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,5 +1,9 @@
 import React from "react";
 import rookiesPic from "../imgs/p2.PNG";
+import pcApp from '../imgs/pcBuild.jpg'
+import tgdPic from '../imgs/sdsd.PNG'
+import pwaPic from '../imgs/textE.PNG'
+import p3 from '../imgs/physics.png'
 
 function IndexPage() {
   return (
@@ -29,10 +33,7 @@ function IndexPage() {
                     can be ready to choose responsibly on draft day even if
                     they’ve had a few too many drafts of their own.
                   </p>
-                  {/* <div class="btn-group flex justify-center">
-          <button class="btn btn-active"><a href = "https://dry-brushlands-86766.herokuapp.com/">Live Link</a></button>
-          <button class="btn"><a href = "https://github.com/Rini0404/rookies">GitHub Repo</a></button>
-        </div> */}
+                
         <div className="flex py-2">
           <div className="px-4">
                   <button
@@ -86,99 +87,164 @@ function IndexPage() {
                   <div>
                     <img
                       className="w-full"
-                      src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(2).png"
+                      src={p3}
                       alt="games"
                     />
-                    <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                      <p className="text-sm text-white font-semibold tracking-wide">
-                        Bruce Wayne
-                      </p>
-                      <p className="text-sm text-white font-semibold tracking-wide">
-                        13TH Oct, 2020
-                      </p>
-                    </div>
                     <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
                       <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
                         Transactions
                       </h1>
                       <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
-                        Find the latest events updates or create events,
-                        concerts, conferences, workshops...
+                        REACT app for Project-3 Will go here!
                       </p>
                     </div>
                   </div>
                   <div>
                     <img
-                      className="w-full"
-                      src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(3).png"
+                      className="w-full rounded"
+                      src={pcApp}
                       alt="notes"
                     />
-                    <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                      <p className="text-sm text-white font-semibold tracking-wide">
-                        Bruce Wayne
-                      </p>
-                      <p className="text-sm text-white font-semibold tracking-wide">
-                        13TH Oct, 2020
-                      </p>
-                    </div>
                     <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
                       <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
-                        Transactions
+                        Pc Architect
                       </h1>
                       <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
-                        Find the latest events updates or create events,
-                        concerts, conferences, workshops...
+                        A MERN App that lets you put together your new Pc Rigg!
+                          Using Api's, E-commerce like website.
                       </p>
+                      <button
+                    type="button"
+                    class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                  >
+                    <a href="https://github.com/Rini0404/pc-architect">
+                      GitHub Repo
+                    </a>
+                    <svg
+                      class="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                  <button
+                    type="button"
+                    class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                  >
+                    <a href = "">Live Link</a>
+                    <svg
+                      class="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
                     </div>
                   </div>
                 </div>
+
                 <div className="mt-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8">
                   <div>
                     <img
-                      className="w-full"
-                      src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(4).png"
+                      className="w-full rounded"
+                      src={tgdPic}
                       alt="laptop"
                     />
-                    <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                      <p className="text-sm text-white font-semibold tracking-wide">
-                        Bruce Wayne
-                      </p>
-                      <p className="text-sm text-white font-semibold tracking-wide">
-                        13TH Oct, 2020
-                      </p>
-                    </div>
                     <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
                       <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
-                        Transactions
+                        That Good Dirt
                       </h1>
                       <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
-                        Find the latest events updates or create events,
-                        concerts, conferences, workshops...
+                       Users should be able
+          to select specific characteristics they would like the trail to accommodate
+          With simple and easy instructions we made it possible for anyone to enjoy mother nature. Creating an
+          interacive experice making it fun from start to finish.
                       </p>
+                      <button
+                    type="button"
+                    class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                  >
+                    <a href="https://github.com/Rini0404/ThatGoodDirt">
+                      GitHub Repo
+                    </a>
+                    <svg
+                      class="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                  <button
+                    type="button"
+                    class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                  >
+                    <a href = "https://rini0404.github.io/ThatGoodDirt/">Live Link</a>
+                    <svg
+                      class="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
                     </div>
                   </div>
                   <div>
                     <img
                       className="w-full"
-                      src="https://cdn.tuk.dev/assets/components/111220/Blg-6/blog(5).png"
+                      src={pwaPic}
                       alt="worker"
                     />
-                    <div className="py-2 px-4 w-full flex justify-between bg-indigo-700">
-                      <p className="text-sm text-white font-semibold tracking-wide">
-                        Bruce Wayne
-                      </p>
-                      <p className="text-sm text-white font-semibold tracking-wide">
-                        13TH Oct, 2020
-                      </p>
-                    </div>
                     <div className="bg-white px-3  lg:px-6 py-4 rounded-bl-3xl rounded-br-3xl">
                       <h1 className="text-lg text-gray-900 font-semibold tracking-wider">
-                        Transactions
+                      Text Editor
                       </h1>
                       <p className="text-gray-700 text-sm  lg:text-base  lg:leading-8 pr-4 tracking-wide mt-2">
-                        Find the latest events updates or create events,
-                        concerts, conferences, workshops...
+                      The app will be a single-page application that meets the PWA criteria. Additionally, it will feature a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser. The application will also function offline.
                       </p>
+                      <button
+                    type="button"
+                    class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
+                  >
+                    <a href="https://github.com/Rini0404/pwa-Text-Editor">
+                      GitHub Repo
+                    </a>
+                    <svg
+                      class="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
                     </div>
                   </div>
                 </div>
