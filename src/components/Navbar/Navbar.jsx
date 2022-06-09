c94489b74b3a235f46bd096e4ff8ec810e014865
Rene Ortega <reneortega2468@gmail.com>
2022-06-09 12:17:48 -0400

    New llok need to work on cards. Not mobile friendly smh...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import resume from "../../WebDev.pdf";
 import { FaHome, FaNewspaper, FaUserCircle, FaChartLine } from 'react-icons/fa'
-import { Outlet, Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Index = () => {
    return(
     
 
             <div class="border-0 what px-7 shadow-lg opacity-80">
-            {/* <img src = {  }/> */}
                 <div class="flex">
                     <div class="flex-1 group text-white">
+                    <Link to="/">
                         <a href="#" class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-white group-hover:text-indigo-500 border-b-2 border-transparent group-hover:border-indigo-500">
                             <span class="block px-1 pt-1 pb-2">
                                 <FaHome class="far fa-compass text-2xl pt-1 mb-1 block"/>
                                 <span class="block text-xs pb-1">Home</span>
                             </span>
                         </a>
+                    </Link>
                     </div>
                     <div class="flex-1 group">
                         <a target = " _blank"href={resume}class="flex items-end justify-center text-center mx-auto px-4 pt-2 w-full text-white group-hover:text-indigo-500 border-b-2 border-transparent group-hover:border-indigo-500">
