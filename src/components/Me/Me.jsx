0a9f76327506aaf4ef472eccb7c6120743d58b8e
rene <rinipini@Rinis-MBP.lan>
2022-07-22 21:38:18 -0400

    edititng..

--- src/components/Me/Me.jsx
+++ src/components/Me/Me.jsx
@@ -9,7 +9,7 @@ function Me() {
         <section className="bgForPjects">
             <div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
                 <div className="backdrop-blur rounded-xl">
-                    <h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl dark:text-gray-50">Let me intorduce <span className="font-bold text-fuchsia-500">Myself</span></h2>
+                    <h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl dark:text-gray-50">Let me introduce <span className="font-bold text-fuchsia-500">Myself</span></h2>
                     <p className="max-w-3xl mx-auto mt-4 text-xl text-center dark:text-gray-400">Rene Ortega, <span className="font-bold text-fuchsia-500">Full-Stack Developer</span></p>
                 </div>
                 <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
